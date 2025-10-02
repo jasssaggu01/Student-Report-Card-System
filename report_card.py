c303cb7b745f46fe9f406bc06d8029b7b5512114
jasssaggu01 <jasssaggu0111@gmail.com>
2025-10-02 10:53:21 +0530

    Update report_card.py

--- report_card.py
+++ report_card.py
@@ -1,1 +1,21 @@
-print("hello, student report card system!")
+# Student Report Card System
+# Author: Jaskaran Singh , Krishna Arora , Garv Sharma , Saransh , Bhupinder Singh 
+def calculate_grade(percentage):
+  if percentage >= 90:
+    return "A+"
+  elif percentage >= 80:
+    return "A"
+  elif percentage >= 70:
+    return "B"
+  elif percentage >= 60:
+    return "C"
+  elif percentage >= 50:
+    return "D"
+  else:
+    return "F (fail)"
+    def report_card():
+      print(Fore.CYAN + "="*45)
+      print(Fore.YELLOW + Style.Bright + "     )
+    
+    
+    
